b206ebb2acb52577b99dbee6c4ca3393cb936a14
Orihuela <norihuela@indracompany.com>
2020-10-29 17:43:12 -0300

    version 4.1

---
+++ src/test/java/pruebas/Test_Esperas.java
@@ -0,0 +1,29 @@
+package pruebas;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Test_Esperas {
+WebDriver driver;
+WebDriverWait espera;
+	@Test
+public void test() {
+	System.setProperty("webdriver.chrome.driver","..\\TestVideo\\Drivers\\chromedriver.exe");
+	driver=new ChromeDriver();
+	driver.get("http://automationpractice.com/index.php");
+	espera=new WebDriverWait(driver,Duration.ofSeconds(20));
+	WebElement textArea = espera.until(ExpectedConditions.presenceOfElementLocated(By.id("search_query_top")));
+	textArea.sendKeys("valor");
+	
+	//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+}
+}
